28e161ef56bd2a21b806f85b70eb1cd94f38111a
Onup <anupme01@gmail.com>
2021-12-16 00:00:23 +0600

    last update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,29 +1,90 @@
 jQuery(document).ready(function(){ 
 
-	// Home owlCarousel
-	jQuery('.blog_slider').owlCarousel({
+	// Brand logo Carousel 
+	jQuery('.slider_item_first').owlCarousel({
 		loop:true,
-		margin:25,
-		nav:true,
+		autoplayTimeout:4000,
 		autoplay:false,
-		navText:['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-		dots:false,
-		responsive:{
+		smartSpeed:4000,
+		stagePadding:330,
+		responsive :{
 			0:{
 				items:1,
 			},
-			576:{
-				items:2,
-			
+			425:{
+				items:1,
 			},
-			768:{
-				items:2,
+			575:{
+				items:1.4,
 			},
-			1200:{
-				items:1.5,
+			767:{
+				items:1.4,
+			},
+			992:{
+				items:1.1,
 			}
 		}
 	});
+	jQuery('.slider_item_first_2').owlCarousel({
+		loop:true,
+		autoplayTimeout:4000,
+		autoplay:false,
+		smartSpeed:4000,
+		stagePadding:230,
+		responsive :{
+			0:{
+				items:1,
+			},
+			425:{
+				items:1,
+			},
+			575:{
+				items:1.4,
+			},
+			767:{
+				items:1.4,
+			},
+			992:{
+				items:1.4,
+			}
+		}
+	});
+	
+
+	jQuery('#pre_btn').click(function(){
+        jQuery('.slider_item_first').trigger('prev.owl.carousel');
+            return false ;
+     });     
+    jQuery('#next_btn').click(function(){       
+        jQuery('.slider_item_first_2').trigger('next.owl.carousel');
+        return false ;          
+    });
+
+	
+	// Home owlCarousel
+	// jQuery('.blog_slider').owlCarousel({
+	// 	loop:true,
+	// 	margin:25,
+	// 	nav:true,
+	// 	autoplay:false,
+	// 	navText:['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+	// 	dots:false,
+	// 	responsive:{
+	// 		0:{
+	// 			items:1,
+	// 		},
+	// 		576:{
+	// 			items:2,
+			
+	// 		},
+	// 		768:{
+	// 			items:2,
+	// 		},
+	// 		1200:{
+	// 			items:1.5,
+	// 		}
+	// 	}
+	// });
 
 
 
